07c1601a68660e1ef05089456e6079ce75fcb177
mobeenaslam007 <mobeenaslam173@gmail.com>
2021-06-14 18:16:58 +0500

    tree, form and search views added

--- models/estate_property.py
+++ models/estate_property.py
@@ -1,19 +1,29 @@
 from odoo import models, fields
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
 
 class EstateProperty(models.Model):
-    _name = "estate.property" # . will be automatically converted into _
+    _name = "estate.property"  # . will be automatically converted into _
     _description = "Info about property"
 
-    name = fields.Char(required=True)
+    title = fields.Char(required=True)
     description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date()
+    postcode = fields.Char(size=10)
+    # copy attribute whether the field value should be copied when the record is duplicated (default: True)
+    date_availability = fields.Date(
+        copy=False, default=date.today() + relativedelta(months=+3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    # default value of readonly is False
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection(selection=[("North", "South"), ("East", "West")])
+    garden_orientation = fields.Selection(selection=[(
+        "north", "North"), ("south", "South"), ("east", "East"), ("west", "West")])
+    active = fields.Boolean(default=False)
+    state = fields.Selection(selection=[("new", "New"), ("offer_received", "Offer Received"), (
+        "offer_accepted", "Offer Accepted"), ("sold", "Sold"), ("cancelled", "Cancelled")])
